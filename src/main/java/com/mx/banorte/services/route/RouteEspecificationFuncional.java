0d110fa8fa25050757b8bb7e7684653153c436a7
GeraldPasten <gerald.pastenn@gmail.com>
2023-06-15 15:36:13 -0400

    v2

--- src/main/java/com/mx/banorte/services/route/RouteEspecificationFuncional.java
+++ src/main/java/com/mx/banorte/services/route/RouteEspecificationFuncional.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.hibernate.HibernateException;
+import com.mx.banorte.services.vo.PerfilUsuario;
 import com.mx.banorte.services.vo.Usuario;
+
 
 @ApplicationScoped
 public class RouteEspecificationFuncional extends RouteBuilder {
@@ -20,10 +25,16 @@ public class RouteEspecificationFuncional extends RouteBuilder {
         @ConfigProperty(name = "delete.usuarios")
         private String delete;
 
+        @Inject
+        @ConfigProperty(name = "select.perfiles.usuario")
+        private String perfiles;
+
+        
+
         @Override
         public void configure() throws Exception {
 
-                //Configuracion de Resr BindingMode para recibir y enviar respuesta JSON
+                //Configuracion de Rest BindingMode para recibir y enviar respuesta JSON
                 restConfiguration().bindingMode(RestBindingMode.json);
 
                 // Definicion del punto REST
@@ -48,41 +59,99 @@ public class RouteEspecificationFuncional extends RouteBuilder {
                         .to("direct:updateUsuario")
 
                         //Creacion de la ruta Camel deleteUsuario
-                        .delete("a/{id}")
+                        .delete("/{id}")
                         .consumes("application/json")
                         .description("Eliminar un usuario")
                         .type(Usuario.class)
-                        .to("direct:deleteUsuario");
+                        .to("direct:deleteUsuario")
 
-                        from("direct:getUserPer")
+                        //Creacion de la ruta Camel getUser
+                        .get("perfiles")
+                        .description("Obtener Usuario y su perfil")
+                        .outType(PerfilUsuario.class)
+                        .to("direct:getUserProfile")
+
+                        //Creacion de la ruta Camel CreatePerfil
+                        .put("perfiles/create")
+                        .description("Crear perfil asociado a un usuario")
+                        .type(PerfilUsuario.class)
+                        .to("direct:createPerfil");
+
+                        from("direct:createPerfil")
+                        .routeId("createUserPerfil")
+                        .log("Creando Perfil asociado a id de usuario: ${body}")
+                        .doTry()
+                        .toD("jpa://" + PerfilUsuario.class.getName())
+                        .doCatch(HibernateException.class, PersistenceException.class)
+                        .log("Error al crear el perfil: ${exception.message}")
+                        .setBody(constant("Error al crear el perfil"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
+                        
+                        from("direct:getUserProfile")
+                        .routeId("getUserPerfil")
+                        .log("Obteniendo Usuarios mediante query ...")
+                        .doTry()
+                        .toD("jpa://" + PerfilUsuario.class.getName() + "?query="+perfiles)
+                        .doCatch(HibernateException.class, PersistenceException.class)
+                        .log("Error al Obtener perfiles de usuario: ${exception.message}")
+                        .setBody(constant("Error al Obtener perfiles de usuario"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
+
+                        from("direct:getUser")
                         .routeId("getUserPerRoute")
                         .log("Obteniendo Usuarios mediante query ...")
+                        .doTry()
                         .toD("jpa://" + Usuario.class.getName()+ "?query="+query)
                         .process(exchange -> {
+                                @SuppressWarnings("unchecked")
                                 List<String[]> resultados = exchange.getIn().getBody(ArrayList.class);
                                 List<Usuario> usuarios = new ArrayList<>();          
                                 for (Object[] resultado :  resultados) {    
                                 Usuario usuario = new Usuario();
-                                usuario.setId(1);
+                                usuario.setId(0);
                                 usuario.setNombre("Su nombre completo es: "+resultado[0].toString() + " " +resultado[1].toString());
-                                usuario.setApellido("Su apellido es: "+resultado[1].toString());
+                                usuario.setApellido("Su apellido modificado es: "+resultado[1].toString() +" Cabrera");
                                 usuarios.add(usuario);
                                 }
                                 exchange.getIn().setBody(usuarios);
-                        });
+                        })
+                        .doCatch(HibernateException.class, PersistenceException.class)
+                        .log("Error al Obtener usuarios: ${exception.message}")
+                        .setBody(constant("Error al obtener usuario"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
 
                         from("direct:crearUsuario")
                         .routeId("crearUsuario")
                         .log("Creando un usuario nuevo: ${body}")
-                        .toD("jpa://" + Usuario.class.getName());
+                        .doTry()
+                        .toD("jpa://" + Usuario.class.getName())
+                        .doCatch(HibernateException.class, PersistenceException.class)
+                        .log("Error al Obtener usuarios: ${exception.message}")
+                        .setBody(constant("Error al crear usuario"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
 
                         from("direct:updateUsuario")
                         .routeId("updateUsuario").log("Probando actualizar usuario")
-                        .to("jpa://"+ Usuario.class.getName() +"?useExecuteUpdate=true");
+                        .doTry()
+                        .to("jpa://"+ Usuario.class.getName() +"?useExecuteUpdate=true")
+                        .log("Error al Obtener usuarios: ${exception.message}")
+                        .setBody(constant("Error al actualizar usuario"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
 
                         from("direct:deleteUsuario")
+                        .doTry()
                         .toD("jpa://"+Usuario.class.getName()+"?query="+delete +"="+"${header.id}&useExecuteUpdate=true")
-                        .log("Deleted usuario: ${body} , ${header.id}");
+                        .doCatch(HibernateException.class, PersistenceException.class)
+                        .log("Error al Obtener usuarios: ${exception.message}")
+                        .setBody(constant("Error al eliminar usuario"))
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+                        .end();
+
 
 
         }  
--- src/main/java/com/mx/banorte/services/vo/PerfilUsuario.java
+++ src/main/java/com/mx/banorte/services/vo/PerfilUsuario.java
@@ -1,22 +1,20 @@
 package com.mx.banorte.services.vo;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity()
-@Table(name = "usuario")
-
-public class Usuario {
-
+@Table(name = "Perfiles")
+public class PerfilUsuario {
+    
     @JsonProperty
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,10 +25,11 @@ public class Usuario {
     private String nombre;
 
     @JsonProperty
-    @Column
-    private String apellido;
+    @ManyToOne()
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
-    public Usuario() {
+    public PerfilUsuario () {
 
     }
 
@@ -50,12 +49,21 @@ public class Usuario {
         this.nombre = nombre;
     }
 
-    public String getApellido() {
-        return apellido;
+    public void getIdInvalido() {
+        System.out.print("Ta malo el id");
+  
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public Usuario getUsuario() {
+        return usuario;
     }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
     
+
+    
+
 }
